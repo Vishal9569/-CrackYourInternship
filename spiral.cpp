93493df4d31d5feda12de2c3b1f5669de33d3492
Vishal kumar <149870754+Vishal9569@users.noreply.github.com>
2024-07-17 21:11:05 +0530

    Add files via upload

---
+++ spiral.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+    vector<int> result;
+    if (matrix.empty())
+        return result;
+
+    int top = 0, bottom = matrix.size() - 1;
+    int left = 0, right = matrix[0].size() - 1;
+
+    while (top <= bottom && left <= right)
+    {
+        // Traverse from left to right along the top row
+        for (int i = left; i <= right; ++i)
+        {
+            result.push_back(matrix[top][i]);
+        }
+        ++top;
+
+        // Traverse from top to bottom along the right column
+        for (int i = top; i <= bottom; ++i)
+        {
+            result.push_back(matrix[i][right]);
+        }
+        --right;
+
+        // Make sure we are still within valid rows
+        if (top <= bottom)
+        {
+            // Traverse from right to left along the bottom row
+            for (int i = right; i >= left; --i)
+            {
+                result.push_back(matrix[bottom][i]);
+            }
+            --bottom;
+        }
+
+        // Make sure we are still within valid columns
+        if (left <= right)
+        {
+            // Traverse from bottom to top along the left column
+            for (int i = bottom; i >= top; --i)
+            {
+                result.push_back(matrix[i][left]);
+            }
+            ++left;
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    // Initialize the matrix
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    // Get the spiral order
+    vector<int> result = spiralOrder(matrix);
+
+    // Print the result
+    for (int num : result)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
